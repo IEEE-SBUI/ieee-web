676e2196ceac4f8fe2ccae554f3b21faeeaeb591
saddamtitanio <saddam.titanio25@gmail.com>
2026-06-06 14:26:02 +0700

    fixed linting configuration, configure workspace scripts

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -1,18 +1,24 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import nextConfig from "eslint-config-next/core-web-vitals";
+import eslintConfigPrettier from "eslint-config-prettier";
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+const eslintConfig = [
+  ...nextConfig,
+  eslintConfigPrettier,
+
+  {
+    languageOptions: {
+      parserOptions: {
+        tsconfigRootDir: import.meta.dirname,
+        project: ["./tsconfig.json"],
+      },
+    },
+  },
+
+  {
+    rules: {
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+    },
+  },
+];
 
 export default eslintConfig;
